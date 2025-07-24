8c171f2935654ef2e512423d126e484e02f5098d
zer0null <zer0nullsec@proton.me>
2025-07-23 20:39:17 -0400

    Bang

--- init.lua
+++ init.lua
@@ -84,6 +84,10 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- My Additions
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
